01442a91fe0545c324da048fcea86d074159d39b
вашКонстантин <KonstantinAchapin@mail.ru>
2019-10-13 09:44:57 +0200

    Провел рефакторинг кода в модуле api

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-// Загружает файлы с удаленного сервера
+// Загружает и отправляет файлы с удаленного сервера и отправляет из формы
 'use strict';
 (function () {
   var UPLOAD_URL = 'https://js.dump.academy/kekstagram';
@@ -7,34 +7,20 @@
   var TIMEOUT = 10000;
 
   window.load = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === STATUS_OK) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIMEOUT;
-
+    var xhr = getXhr(onSuccess, onError);
     xhr.open('GET', LOAD_URL);
     xhr.send();
   };
 
   window.send = function (data, onSuccess, onError) {
+    var xhr = getXhr(onSuccess, onError);
+    xhr.open('POST', UPLOAD_URL);
+    xhr.send(data);
+  };
+
+  var getXhr = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
+
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
@@ -55,8 +41,7 @@
 
     xhr.timeout = TIMEOUT;
 
-    xhr.open('POST', UPLOAD_URL);
-    xhr.send(data);
+    return xhr;
   };
 
 })();
